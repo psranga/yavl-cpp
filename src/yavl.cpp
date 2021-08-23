65274c8eced519e84640678ea4989348ef6e973b
Enrique Samson Jr <enriquejr@gmail.com>
2021-08-23 11:49:36 +0800

    Bring up yaml-cpp dependency to v0.7.0

--- src/yavl.cpp
+++ src/yavl.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <assert.h>
-#include "yaml.h"
+#include "yaml-cpp/yaml.h"
 #include "yavl.h"
 
 using namespace std;
@@ -68,36 +68,39 @@ ostream& operator << (ostream& os, const Errors& v)
   return os;
 }
 
-const string& Validator::type2str(YAML::CONTENT_TYPE t)
+const string& Validator::type2str(YAML::NodeType::value t)
 {
-  static string nonestr = "none";
+  static string undefinedstr = "undefined";
+  static string nullstr = "null";
   static string scalarstr = "scalar";
   static string liststr = "list";
   static string mapstr = "map";
 
-  assert( (t >= YAML::CT_NONE) && (t <= YAML::CT_MAP) );
+  assert( (t >= YAML::NodeType::Undefined) && (t <= YAML::NodeType::Map) );
 
   switch (t) {
-    case YAML::CT_NONE:
-      return nonestr;
-    case YAML::CT_SCALAR:
+    case YAML::NodeType::Undefined:
+      return undefinedstr;
+    case YAML::NodeType::Null:
+      return nullstr;
+    case YAML::NodeType::Scalar:
       return scalarstr;
-    case YAML::CT_SEQUENCE:
+    case YAML::NodeType::Sequence:
       return liststr;
-    case YAML::CT_MAP:
+    case YAML::NodeType::Map:
       return mapstr;
   }
   assert(0);
-  return nonestr;
+  return undefinedstr;
 }
 
 int Validator::num_keys(const YAML::Node& doc)
 {
-  if (doc.GetType() != YAML::CT_MAP) {
+  if (doc.Type() != YAML::NodeType::Map) {
     return 0;
   }
   int num = 0;
-  for (YAML::Iterator i = doc.begin(); i != doc.end(); ++i) {
+  for (YAML::const_iterator i = doc.begin(); i != doc.end(); ++i) {
     num++;
   }
   return num;
@@ -105,18 +108,18 @@ int Validator::num_keys(const YAML::Node& doc)
 
 bool Validator::validate_map(const YAML::Node &mapNode, const YAML::Node &doc)
 {
-  if (doc.GetType() != YAML::CT_MAP) {
-    string reason = "expected map, but found " + type2str(doc.GetType());
+  if (doc.Type() != YAML::NodeType::Map) {
+    string reason = "expected map, but found " + type2str(doc.Type());
     gen_error(Exception(reason, gr_path, doc_path));
     return false;
   }
 
   bool ok = true;
-  for (YAML::Iterator i = mapNode.begin(); i != mapNode.end(); ++i) {
-    string key = i.first();
-    const YAML::Node &valueNode = i.second();
-    const YAML::Node *docMapNode = 0;
-    if (!(docMapNode = doc.FindValue(key))) {
+  for (YAML::const_iterator i = mapNode.begin(); i != mapNode.end(); ++i) {
+    string key = i->first.as<string>();
+    const YAML::Node &valueNode = i->second;
+    YAML::Node docMapNode;
+    if (!(docMapNode = doc[key])) {
       string reason = "key: " + key + " not found.";
       gen_error(Exception(reason, gr_path, doc_path));
       ok = false;
@@ -124,7 +127,7 @@ bool Validator::validate_map(const YAML::Node &mapNode, const YAML::Node &doc)
       doc_path.push_back(key);
       gr_path.push_back(key);
 
-      ok = validate_doc(valueNode, *docMapNode) && ok;
+      ok = validate_doc(valueNode, docMapNode) && ok;
 
       gr_path.pop_back();
       doc_path.pop_back();
@@ -135,13 +138,13 @@ bool Validator::validate_map(const YAML::Node &mapNode, const YAML::Node &doc)
 
 bool Validator::validate_leaf(const YAML::Node &gr, const YAML::Node &doc)
 {
-  assert( gr.GetType() == YAML::CT_SEQUENCE );
+  assert( gr.Type() == YAML::NodeType::Sequence );
 
   const YAML::Node& typespec_map = gr[0];
   assert( num_keys(typespec_map) == 1);
 
-  string type = typespec_map.begin().first();
-  const YAML::Node& type_specifics = typespec_map.begin().second();
+  string type = typespec_map.begin()->first.as<string>();
+  const YAML::Node& type_specifics = typespec_map.begin()->second;
 
   bool ok = true;
   if (type == "string") {
@@ -156,9 +159,9 @@ bool Validator::validate_leaf(const YAML::Node &gr, const YAML::Node &doc)
     attempt_to_convert<unsigned int>(doc, ok);
   } else if (type == "enum") {
     ok = false;
-    string docValue = doc;
-    for (YAML::Iterator i = type_specifics.begin(); i != type_specifics.end(); ++i) {
-      if (*i == docValue) {
+    string docValue = doc.as<std::string>();
+    for (YAML::const_iterator i = type_specifics.begin(); i != type_specifics.end(); ++i) {
+      if (i->as<std::string>() == docValue) {
         ok = true;
         break;
       }
@@ -173,8 +176,8 @@ bool Validator::validate_leaf(const YAML::Node &gr, const YAML::Node &doc)
 
 bool Validator::validate_list(const YAML::Node &gr, const YAML::Node &doc)
 {
-  if (doc.GetType() != YAML::CT_SEQUENCE) {
-    string reason = "expected list, but found " + type2str(doc.GetType());
+  if (doc.Type() != YAML::NodeType::Sequence) {
+    string reason = "expected list, but found " + type2str(doc.Type());
     gen_error(Exception(reason, gr_path, doc_path));
     return false;
   }
@@ -183,7 +186,7 @@ bool Validator::validate_list(const YAML::Node &gr, const YAML::Node &doc)
   int n = 0;
   char buf[128];
 
-  for (YAML::Iterator i = doc.begin(); i != doc.end(); ++i, ++n) {
+  for (YAML::const_iterator i = doc.begin(); i != doc.end(); ++i, ++n) {
     snprintf(buf, sizeof(buf), "[%d]", n);
     doc_path.push_back(buf);
     ok = validate_doc(gr, *i) && ok;
@@ -195,15 +198,15 @@ bool Validator::validate_list(const YAML::Node &gr, const YAML::Node &doc)
 bool Validator::validate_doc(const YAML::Node &gr, const YAML::Node &doc)
 {
   bool ok = true;
-  const YAML::Node *mapNode = 0;
-  const YAML::Node *listNode = 0;
-  if ((mapNode = gr.FindValue("map"))) {
+  YAML::Node mapNode;
+  YAML::Node listNode;
+  if (gr["map"] && (mapNode = gr["map"])) {
     gr_path.push_back("map");
-    ok = validate_map(*mapNode, doc) && ok;
+    ok = validate_map(mapNode, doc) && ok;
     gr_path.pop_back();
-  } else if ((listNode = gr.FindValue("list"))) {
+  } else if (gr["list"] && (listNode = gr["list"])) {
     gr_path.push_back("list");
-    ok = validate_list(*listNode, doc) && ok;
+    ok = validate_list(listNode, doc) && ok;
     gr_path.pop_back();
   } else {
     ok = validate_leaf(gr, doc) && ok;
